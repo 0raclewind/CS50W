75347f27e5177186673fa2578d828d2907a0b2df
0raclewind <look4award@yahoo.com>
2021-03-13 06:47:39 +0000

    Added bid model

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -14,13 +14,18 @@ class Listing(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     watchers = models.ManyToManyField(User, blank=True, related_name="watchers")
     time_created = models.DateTimeField(auto_now=True)
-    # auction_duration = models.PositiveSmallIntegerField()
     title = models.CharField(max_length=40)
     description = models.TextField(max_length=400)
-    starting_bid = models.DecimalField(max_digits=5, decimal_places=2)
-    current_bid = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    starting_bid = models.FloatField()
+    current_bid = models.FloatField(blank=True, null=True)
     image_url = models.CharField(max_length=200, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.title} - {self.starting_bid}"
+
+class Bid(models.Model):
+    bidder = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bidder")
+    auction = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="auction")
+    offer = models.FloatField()
+    timestamp = models.DateTimeField(auto_now=True)
